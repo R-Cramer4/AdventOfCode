21ca243a4decb36517fb7ddf45e0d27d32a7a496
penguin-dude123 <73714848+penguin-dude123@users.noreply.github.com>
2023-12-10 12:59:52 -0500

    rust day 1 pt 2 done

--- day01/rust01/src/main.rs
+++ day01/rust01/src/main.rs
@@ -1,22 +1,37 @@
-use std::fs;
+use std::{fs, usize};
 
 fn main() {
     let contents = read_lines("input.txt");
     let mut values = 0;
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     for line in contents{
-        let size = line.chars();
         let mut calibration: (u32, u32) = (0, 0);
-        for ch in size {
+        for ch in 0..line.len() {
+            for number in 0..numbers.len() as u32 {
+                if line.len() < ch + numbers[number as usize].len() {continue;}
+                if line[ch .. numbers[number as usize].len() + ch] == *numbers[number as usize] {
+                    if calibration.0 == 0 {
+                        calibration.0 = number;
+                    }
+                    calibration.1 = number;
+                }
+            }
             if calibration.0 == 0{
-                calibration.0 = match ch.to_digit(10){
+                calibration.0 = match line.chars().nth(ch) {
                     None => 0,
-                    Some(num) => num
+                    Some(ch) => match ch.to_digit(10){
+                        None => 0,
+                        Some(num) => num
+                    }
                 };
             }
-            calibration.1 = match ch.to_digit(10){
+            calibration.1 = match line.chars().nth(ch) {
                 None => calibration.1,
-                Some(num) => num
-            }
+                Some(ch) => match ch.to_digit(10){
+                    None => calibration.1,
+                    Some(num) => num
+                }
+            };
         }
         values += (calibration.0 * 10) + calibration.1;
     }
